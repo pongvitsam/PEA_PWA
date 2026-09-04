9393dedcb1a7cbc71643c7b97b5f7af45993fd8e
pongvitsam <pongvitsam@users.noreply.github.com>
2026-09-04 14:59:38 +0000

    deploy: 24f683da0cbb5338a175e795dd093a3c9019cadf

---
+++ js/config.js
@@ -0,0 +1,4 @@
+/** Google Apps Script Web App URL (ลิงก์เดิมยังใช้ได้) */
+if (typeof window !== 'undefined') {
+  window.GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwEIi5cZDzvdGqcfqcsJcPjW1pBnTALtZFlGYZDkCYl9MTvOL0wuv4mBOEny4UUzyk9/exec';
+}
